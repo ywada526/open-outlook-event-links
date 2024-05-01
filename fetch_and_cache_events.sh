da6026e548f1975219a135cbb2900267b66a2a29
ywada526 <29218439+ywada526@users.noreply.github.com>
2024-05-01 10:44:49 +0900

    If the cache file is empty, it is assumed that there is no cache

--- fetch_and_cache_events.sh
+++ fetch_and_cache_events.sh
@@ -4,7 +4,7 @@ script_dir=$(dirname "$(realpath "$0")")
 cache_file="$script_dir/cache/events.json"
 today=$(date "+%Y-%m-%d")
 
-if [ -f "$cache_file" ] && [ "$(date -r "$cache_file" "+%Y-%m-%d")" = "$today" ]; then
+if [ -s "$cache_file" ] && [ "$(date -r "$cache_file" "+%Y-%m-%d")" = "$today" ]; then
   cat "$cache_file"
   "$script_dir"/fetch_events.sh > "$cache_file" &
 else
